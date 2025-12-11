55e77e3bcf9be37b53bdde28f548837a939f18b7
Shruti Salve <104889266+shruti-00@users.noreply.github.com>
2025-12-11 10:23:13 +0530

    Update main.tf

--- child-module/vpc/main.tf
+++ child-module/vpc/main.tf
@@ -1,7 +1,16 @@
-resource "aws_vpc" "var.name" {
-  cidr_block           = var.cidr_block
-  tags = merge(
-    var.tags,
-    { Name = "${var.name}-vpc" }
-  )
+#resource "aws_vpc" "var.name" {
+#  cidr_block           = var.cidr_block
+#  tags = merge(
+#    var.tags,
+#    { Name = "${var.name}-vpc" }
+#  )
+#
+#}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = var.name
+  }
 }
