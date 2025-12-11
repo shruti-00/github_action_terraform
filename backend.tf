ce30a0c32b1d4d48fe263ef9ddd31a47c5fb2c8f
Shruti Salve <104889266+shruti-00@users.noreply.github.com>
2025-12-11 10:33:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-s3-backend-amdocs"
-    key    = "env/prod/"
+    key    = "env/prod/terraform.tfstate"
     region = "ap-south-1"
     #use_lockfile = "true"
   }
